d6b9058d38c0638d93061c0ab472bf3e9cfb5e8f
nasty <nastyakras1707@gmail.com>
2024-02-08 15:11:58 +0300

    Добавьте файлы проекта.

---
+++ Library/CheckClass.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Library
+{
+    public class CheckClass
+    {
+
+        public static bool IsDBConnected(string connectionString)
+        {
+            var connection = new NpgsqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public static bool IsEmailCorrect(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+
+        public static bool IsPhoneNumberCorrect(string number)
+        {
+            if (string.IsNullOrWhiteSpace(number))
+                return false;
+
+            return Regex.IsMatch(number, @"^8-9[0-9]{2}-[0-9]{3}-[0-9]{2}-[0-9]{2}$");
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void IsDbConnected_true()
+        {
+            string con = "Host=localhost:5432; Username=postgres; Password=25481; Database=UserDatabase";
+            bool expected = true;
+            bool actual = CheckClass.IsDBConnected(con);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsDbConnected_false()
+        {
+            string con = "Host=localhost:5432; Username=postgres; Password=12345; Database=UserDatabase";
+            bool expected = false;
+            bool actual = CheckClass.IsDBConnected(con);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsEmailCorrect_true()
+        {
+            string email = "ivanov@mail.com";
+            bool expected = true;
+            bool actual = CheckClass.IsEmailCorrect(email);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsEmailCorrect_false()
+        {
+            string email = "ivanov_mail.com";
+            bool expected = false;
+            bool actual = CheckClass.IsEmailCorrect(email);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPhoneNumberCorrect_true()
+        {
+            string number = "8-952-123-78-98";
+            bool expected = true;
+            bool actual = CheckClass.IsPhoneNumberCorrect(number);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsPhoneNumberCorrect_false()
+        {
+            string number = "+7 (950) 987 12 23";
+            bool expected = false;
+            bool actual = CheckClass.IsPhoneNumberCorrect(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
